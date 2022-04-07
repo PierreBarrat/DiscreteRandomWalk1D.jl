d608ec7bc618781d46047967614eeea934894d2a
PierreBarrat <p.barrat@live.fr>
2022-04-07 17:49:15 +0200

    works

--- src/objects.jl
+++ src/objects.jl
@@ -22,23 +22,41 @@ x::Float64 = 0.
 @with_kw mutable struct EFRW <: RandomWalker
 	s::Float64 = 0.01
 	α::Float64 = 0.01
-	β::Float64 = 1. - exp(-s/α)
+	β::Distribution = Dirac(1. - exp(-s/α))
+	ρ::Float64 = 1.
+	μ::Float64 = 0.
 	t::Int = 0
 	x::Float64 = 0.
 end
+"""
+	EFRW(β; kwargs...)
 
-p_right(rw::EFRW) = rw.x
-p_left(rw::EFRW) = 1. - rw.x
-step_size_right(rw::EFRW) = rw.β * (1. - rw.x)
-step_size_left(rw::EFRW) = rw.β * rw.x
+Create an `EFRW` object with the amplitude of partial sweeps distributed according to `β`.
+If `β::Number`, the distribution will be `Dirac(β)`.
+
+For a description of keyword arguments, see the help for the struct `EFRW`.
+"""
+EFRW(β::Distribution; kwargs...) = EFRW(; β, kwargs...)
+EFRW(β::Number; kwargs...) = EFRW(; β = Dirac(β), kwargs...)
 
 function step!(rw::EFRW)
-	p = rand()
-	if p <= rw.x
-		rw.x += rw.β * (1. - rw.x)
-	else
-		rw.x -= rw.β * rw.x
+	# Mutation is deterministic. To make it stochastic, I would need the population size
+	if rw.μ > 0
+		rw.x += rw.μ * (1 - 2*rw.x)
 	end
+
+	# Partial sweep
+	p = rand()
+	if p < rw.ρ # Is there a partial sweep?
+		p = rand()
+		βval = rand(rw.β)
+		if p <= rw.x
+			rw.x += βval * (1. - rw.x)
+		else
+			rw.x -= βval * rw.x
+		end
+	end
+
 	rw.t += 1
 	return rw
 end
@@ -46,79 +64,6 @@ end
 position(rw::EFRW) = rw.x
 pos(rw::EFRW) = rw.x
 
-"""
-	mutable struct StochasticEFRW
-
-Expiring Fitness Random Walker with a randomly chosen β.
-
-## Fields
-
-```
-β::Distribution = (1. - exp(-sval/α)) * DiscreteUniform(1,1)
-βval::Float64 = rand(β)
-t::Int = 0
-x::Float64 = 0.
-```
-"""
-@with_kw mutable struct StochasticEFRW <: RandomWalker
-	β::Distribution = (1. - exp(-sval/α)) * DiscreteUniform(1,1)
-	βval::Float64 = rand(β)
-	t::Int = 0
-	x::Float64 = 0.
-end
-function StochasticEFRW(β::Float64; kwargs...)
-	return StochasticEFRW(β = β*DiscreteUniform(1,1); kwargs...)
-end
-
-function step!(rw::StochasticEFRW)
-	p = rand()
-	if p <= rw.x
-		rw.x += rw.βval * (1. - rw.x)
-	else
-		rw.x -= rw.βval * rw.x
-	end
-	rw.t += 1
-	# Update β
-	rw.βval = rand(rw.β)
-
-	return rw
-end
-
-position(rw::StochasticEFRW) = rw.x
-pos(rw::StochasticEFRW) = rw.x
-
-
-# """
-# 	mutable struct SymEFRW
-
-# Approximation to `EFRW` with a symetric step size.
-
-# ## Fields
-
-# ```
-# s::Float64 = 0.01
-# α::Float64 = 0.01
-# γ::Float64 = exp(-s/α)
-# t::Int = 0
-# x::Float64 = 0.
-# ```
-# """
-# @with_kw mutable struct SymEFRW <: RandomWalker
-# 	s::Float64 = 0.01
-# 	α::Float64 = 0.01
-# 	γ::Float64 = exp(-s/α)
-# 	#
-# 	t::Int = 0
-# 	x::Float64 = 0.5
-# end
-
-# p_right(rw::SymEFRW) = (1 + (1-2*rw.x)/sqrt(2*((1-rw.x)^2 + rw.x^2)))/2
-# p_left(rw::SymEFRW) = 1. - p_right(rw)
-
-# step_size_right(rw::SymEFRW) = (1-rw.γ) * sqrt(((1-rw.x)^2 + rw.x^2)/2)
-# step_size_left(rw::SymEFRW) = step_size_right(rw)
-
-# update!(::SymEFRW) = nothing
 
 ######################################################################
 ######################### Neutral random walk ########################
